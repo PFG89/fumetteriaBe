f095915d584117ce4968dc540334a5fa66f31fc3
Pier Francesco Grande <pieroliberi@gmail.com>
2022-09-06 20:00:17 +0200

    first commit

---
+++ src/main/java/school/devskill/Fumetteria/FumetteriaApplication.java
@@ -0,0 +1,13 @@
+package school.devskill.Fumetteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FumetteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FumetteriaApplication.class, args);
+	}
+
+}
